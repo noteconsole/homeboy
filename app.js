4c20c2273171172305524d68f92ade016b6f1c02
noteconsole <rtanguayjohn@gmail.com>
2023-04-23 16:11:50 -0400

    been like this for a minute

--- app.js
+++ app.js
@@ -19,56 +19,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 const Prismic = require('@prismicio/client')
 const PrismicDOM = require('prismic-dom')
+const { internalIP } = require('webpack-dev-server')
 
 // Initialize the prismic.io api
 const initApi = req => {
   return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
-		accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
     req
   })
 }
 
-const noteTopics = ["Debt", "Film", "Mets", "Music", "Other", "Projects", "Reading", "SCF"]
-
-function noteMatch(value) {
-
-	if (value == 'Debt') {
-		return '/notes/debt'
-	}
-
-	if (value == 'Film') {
-		return '/notes/film'
-	}
-
-	if (value == 'Mets') {
-		return '/notes/mets'
-	}
-
-	if (value == 'Music') {
-		return '/notes/music'
-	}
-
-	if (value == 'Other') {
-		return '/notes/other'
-	}
-
-	if (value == 'Projects') {
-		return '/notes/projects'
-	}
-
-	if (value == 'Reading') {
-		return '/notes/reading'
-	}
-
-	if (value == 'SCF') {
-		return '/notes/scf'
-	}
-
-}
-
 // Link Resolver
 function handleLinkResolver(note, noteTopics) {
-	noteTopics.forEach(noteMatch)
 	if (doc.type === 'product') {
 		return `/detail/${doc.slug}`
 	}
@@ -96,15 +58,7 @@ function handleLinkResolver(note, noteTopics) {
 app.use((req, res, next) => {
 
 	res.locals.ctx = {
-    endpoint: process.env.PRISMIC_ENDPOINT,
-    linkResolver: handleLinkResolver
-	}
-
-	//res.locals.Notes = notes = ["Debt", "Film", "Mets", "Music", "Other", "Projects", "Reading", "SCF"];
-	//res.locals.Link = handleLinkResolver
-
-	res.locals.Numbers = index => {
-		return index == 0 ? 'One' : index == 1 ? 'Two' : index == 2 ? 'Three' : index == 3 ? 'Four' : '';
+    endpoint: process.env.PRISMIC_ENDPOINT
 	}
 
 	res.locals.PrismicDOM = PrismicDOM
@@ -114,7 +68,7 @@ app.use((req, res, next) => {
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
-/*
+
 const handleRequest = async api => {
 	const meta = await api.getSingle('metadata')
 
@@ -122,57 +76,17 @@ const handleRequest = async api => {
 		meta
 	}
 }
-*/
 
-app.get('/', (req, res) => {
-	//const api = await initApi(req)
-	//const defaults = await handleRequest(api)
-	//const home = await api.getSingle('home')
+app.get('/', async (req, res) => {
+	const api = await initApi(req)
+  const defaults = await handleRequest(api)
 
-  res.render('pages/home', {title: "Home"})
-})
+	//console.log(defaults)
 
-app.get('/about', (req, res) => {
-	//const api = await initApi(req)
-	//const defaults = await handleRequest(api)
-	//const home = await api.getSingle('home')
-
-  res.render('pages/about', {title: "About"})
-})
-
-app.get('/notes', async (req, res) => {
-	//const api = await initApi(req)
-	//const defaults = await handleRequest(api)
-	//const note = await api.getSingle('note')
-
-  res.render('pages/blog', {
-		title: "Notes"
+  res.render('pages/home', {
+		defaults,
+		title: "A foregone conclusion"
 	})
-})
-
-app.get('/notes/:id', async (req, res) => {
-
-	res.render('pages/notes/' + req.params.id, {
-		title: req.params.id,
-	})
-
-	/*
-	const notes = handleLinkResolver(noteTopics)
-
-  res.render('pages/notes_breakout/:note', {
-		title: "Notes",
-		vals: vals,
-		notes: notes,
-	})
-	*/
-})
-
-app.get('/contact', (req, res) => {
-	//const api = await initApi(req)
-	//const defaults = await handleRequest(api)
-	//const home = await api.getSingle('home')
-
-  res.render('pages/contact', {title: "Contact"})
 })
 
 app.listen(port, () => {
